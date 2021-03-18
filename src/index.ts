33fce2146c213134e91d84e56040732f3ff07605
Amine Benseddik <amine@pixelfactory.io>
2021-03-18 14:34:56 +0100

    chore: refact index.ts
    * change initialization order
    * handle browserHandler events

--- src/index.ts
+++ src/index.ts
@@ -15,28 +15,42 @@ const logger = createLogger({
 });
 
 const wsEndpoint = process.env.WS_ENDPOINT || 'ws://localhost:3000';
-const browserMaxConnRetry = process.env.BROWSER_MAX_CONNECTION_RETRY || '20';
+const browserMaxConnRetry = process.env.BROWSER_MAX_CONNECTION_RETRY || '5';
+
+/**
+ * Initialize BrowserHandler
+ */
+const bh = new BrowserHandler(
+  BrowserHandler.WithWsEndpoint(wsEndpoint),
+  BrowserHandler.WithMaxConnectionRetry(Number(browserMaxConnRetry)),
+);
+
+bh.on('error', (e) => {
+  logger.error('an error happened', { err: e });
+  process.exit(1);
+});
+
+bh.on('connection_retry', (e, attemptNumber: number) => {
+  logger.warn(`trying to reconnect browser ${wsEndpoint}`, { err: e, attemptNumber: attemptNumber });
+});
+
+/**
+ * Initialize Probe
+ */
+const probe = new Probe(Probe.WithLogger(logger), Probe.WithBrowserHandler(bh));
+
+/**
+ * Initialize App
+ */
+const app = new App({
+  port: Number(process.env.PORT) || 8090,
+  logger: logger,
+  middlewares: [LoggerMiddleware(logger)],
+  errorMiddlewares: [ErrorMiddleware(logger)],
+  controllers: [new Home(), probe],
+});
 
 (async (): Promise<void> => {
-  const app = new App({
-    port: Number(process.env.PORT) || 8090,
-    logger: logger,
-    middlewares: [LoggerMiddleware(logger)],
-    errorMiddlewares: [ErrorMiddleware(logger)],
-    controllers: [
-      new Home(),
-      new Probe(
-        Probe.WithLogger(logger),
-        Probe.WithBrowserHandler(
-          new BrowserHandler(
-            BrowserHandler.WithLogger(logger),
-            BrowserHandler.WithWsEndpoint(wsEndpoint),
-            BrowserHandler.WithMaxConnectionRetry(Number(browserMaxConnRetry)),
-          ),
-        ),
-      ),
-    ],
-  });
-
+  await bh.initBrowser();
   app.listen();
 })();
